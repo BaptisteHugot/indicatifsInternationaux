1c36da38ff6d76c1887b5cd4520be9abe1fd6b00
Baptiste Hugot <19981614+BaptisteHugot@users.noreply.github.com>
2021-05-24 16:50:21 +0200

    Ajout de la gestion des cartes SIM

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,18 +4,18 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e63332689d1466c293c5dde96c1c576c",
-"index.html": "1d4d33c45ca8ec3e5e3632d08be42c65",
-"/": "1d4d33c45ca8ec3e5e3632d08be42c65",
-"main.dart.js": "8ca64797ec5fa22ea258d862e9daf0bc",
+"index.html": "c8a44fefe46ce33f0265c108740a207b",
+"/": "c8a44fefe46ce33f0265c108740a207b",
+"main.dart.js": "42596f78c4f9da3395838774a55adea6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "7d5a5282760e5775457365c4b8aff7a2",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "74c5f85038ef0bed743eaf4bf8c55147",
+"assets/NOTICES": "31f435a8de2e5fb19c10207bb4083859",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -33,7 +33,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
